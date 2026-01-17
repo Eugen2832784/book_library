bfd0397b87bfecc765da46dfe7fcda73a3413e8e
EugeneDev <kovalonokjevgenij@gmail.com>
2026-01-17 16:41:40 +0200

    add modal window for input

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const myLibrary = [];
+document.querySelector("#openModal").addEventListener("click", () => {
+  document.querySelector("#modal").showModal();
+});
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, read) {
+  if (!new.target) {
+    throw Error("You must use the 'new' operator to call the constructor");
+  }
+  this.id = crypto.randomUUID();
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
 }
 
 function addBookToLibrary() {
